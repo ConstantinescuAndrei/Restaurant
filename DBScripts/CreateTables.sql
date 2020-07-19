eae4914f0911ba8f60319221fe0e4ffcbc117e0b
miutamihai <miuta.mihai@gmail.com>
2020-07-19 23:27:30 +0300

    Create basic backend structure

---
+++ DBScripts/CreateTables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Items(
+	Id int primary key identity,
+	Name varchar(200),
+	ImageName varchar(200),
+	PricePerUnit int
+);
+
+CREATE TABLE Users(
+	Id int primary key identity,
+	UserName varchar(200),
+	Email varchar(200),
+	Password varchar(200),
+	Address varchar(200)
+);
+
+CREATE TABLE Cart(
+	Id int primary key identity,
+	UserId int references Users(Id)
+); 
+
+CREATE TABLE Content(
+	Id int primary key identity,
+	ItemId int references Items(Id),
+	ItemQuantity int,
+	CartId int references Cart(Id)
+);
+
+ALTER TABLE Users WITH CHECK
+	ADD CONSTRAINT UQ_Users_Email UNIQUE (Email),
+	ADD CONSTRAINT UQ_Users_UserName UNIQUE (UserName)
+
