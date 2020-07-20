91351368f098ce7c4aeb91b1af2d5665bdbc2eed
miutamihai <miuta.mihai@gmail.com>
2020-07-20 09:08:02 +0300

    Fix requirements

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import mysql.connector
+
+conn = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Virtual1234",
+  port="4000"
+)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return "Hi there"
+    cursor = conn.cursor()
+    cursor.execute("call Restaurant.Register('test', 'test', 'test', 'test');")
+
+    res = cursor.fetchone()
+    print(res)
+    return res[0]
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    cursor = conn.cursor()
+    cursor.execute(request.form['UserName'], request.form['Email'], request.form['Password'], request.form['Address'])
 
 
 if __name__ == '__main__':
